b242316d196594cefba97139e8730d38a3164ee7
hakantozkoparan <hakantozkoparan@icloud.com>
2024-03-18 10:33:58 +0300

    KategoriController ve Views yapıldı.

--- MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KategoriController.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Siniflar;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
+        {
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult KategoriEkle()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategori k)
+        {
+            c.Kategoris.Add(k);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kate = c.Kategoris.Find(id);
+            c.Kategoris.Remove(kate);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kate = c.Kategoris.Find(id);
+            return View("KategoriGetir", kate);
+        }
+
+        public ActionResult KategoriGuncelle(Kategori k)
+        {
+            var kate = c.Kategoris.Find(k.Id);
+            kate.Ad = k.Ad;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }   
     }
 }
